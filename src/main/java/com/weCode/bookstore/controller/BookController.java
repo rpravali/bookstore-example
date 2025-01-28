be0d8340d50e2997d776d14ad0b2eceeae403ee9
rpravali <89647671+rpravali@users.noreply.github.com>
2025-01-28 11:16:27 +0530

    changed name

--- src/main/java/com/weCode/bookstore/controller/BookController.java
+++ src/main/java/com/weCode/bookstore/controller/BookController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/books")
+@RequestMapping("api/v1/book")
 public class BookController {
 
     @GetMapping
